598ba8c72b0cba78de97a681b3cbe06e11b953f3
ammererjak0b <jakob.ammerer@hotmail.com>
2026-04-08 11:22:29 +0200

    after lab02

--- platformioconfig-main/platformioconfig-main/src/main.c
+++ platformioconfig-main/platformioconfig-main/src/main.c
@@ -1,13 +1,25 @@
 #include "avrhal/ugly-usart.h"
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <string.h>
 
 #define LOOP_DELAY_MS 500
 
 int main(void)
 {
     usartSetup(9600);
-    uint8_t message[] = "Wir Brauchen diese Serielle Schnittstelle das nächste mal auch noch.\n\r";
+    uint8_t laborUebung = 3;
+    uint8_t message[255];
+
+    char * cow_string = 
+    "  ^__^\n\r"
+    "  (oo)\\_______\n\r"
+    "  (__)\\       )\\/\\\n\r"
+    "      ||----w |\n\r"
+    "      ||     ||\n\r";
+
+    snprintf(message, 255, "%s", cow_string);
+
     sei();
     while (1) {
         _delay_ms(LOOP_DELAY_MS); 
